79aa291c05e326a90bb45f2c536a10e582581d64
Subham Sahu <about.subhamsahu@gmail.com>
2022-12-03 00:12:39 +0530

    03rd Dec 2022 - API Routes Setup

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,8 +1,8 @@
-const uuid = require("uuid");
+const {v4 : uuidv4} = require("uuid");
 
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
@@ -29,26 +29,26 @@ const getPlaceById = (req, res, next) => {
   res.json({ place: place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
+  const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     return next(
-      new HttpError("Could not find a place for the provided user id", 404)
+      new HttpError("Could not find places for the provided user id", 404)
     );
   }
 
-  res.json({ place: place });
+  res.json({ places: places });
 };
 
 const createPlace = (req, res, next) => {
   const { title, description, coordinates, address, creator } = req.body;
 
   const createdPlace = {
-    id: uuid(),
+    id: uuidv4(),
     title: title,
     description: description,
     location: coordinates,
@@ -61,6 +61,28 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatePlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  updatePlace.title = title;
+  updatePlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatePlace;
+
+  res.status(200).json({ place: updatePlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+  res.status(200).json({message: 'Deleted place'})
+};
+
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
